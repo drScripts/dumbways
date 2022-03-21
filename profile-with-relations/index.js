9c6a54d42ea5ac6d3e31cfe0a4e6cab32d80cd29
Nathanael <nathanael.vd@gmail.com>
2022-03-21 21:58:33 +0700

    Change session config

--- profile-with-relations/index.js
+++ profile-with-relations/index.js
@@ -37,8 +37,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      httpOnly: !appSecure,
-      secure: appSecure,
+      httpOnly: appSecure,
     },
   })
 );
